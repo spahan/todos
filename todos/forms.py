83e0e4db82a0d4c40bae17dc7c36b1cdeeed2707
Hanspeter Spalinger <github@spahan.ch>
2019-12-11 12:24:04 +0100

    refactor: more code style stuff

--- todos/forms.py
+++ todos/forms.py
@@ -1,31 +1,28 @@
-import datetime
-
 from flask_wtf import FlaskForm
-#from flask_wtf.file import FileField
-from wtforms import StringField, SelectField, TextAreaField, SubmitField, DateTimeField
-#from wtforms.fields import BooleanField
-#from wtforms.fields.html5 import TimeField
-from wtforms.validators import *
-
-# DateField from wtforms_components supports min/max depending on DateRange
+from wtforms import SelectField, StringField, TextAreaField
+from wtforms.validators import DataRequired, Email
 from wtforms_components import DateField
-#from wtforms_components import DateRange
 
 
 class LoginForm(FlaskForm):
-    login = StringField('Email', validators=[DataRequired(), Email(message='Please enter valid emailaddress')])
+    login = StringField(
+        "Email",
+        validators=[DataRequired(), Email(message="Please enter valid emailaddress")],
+    )
 
 
 class TodoForm(FlaskForm):
-    title = StringField('Title', validators=[DataRequired()])
-    description = TextAreaField('Description', validators=[DataRequired()])
+    title = StringField("Title", validators=[DataRequired()])
+    description = TextAreaField("Description", validators=[DataRequired()])
 
-    due_date = DateField('Due date')
-    priority = SelectField('Priority', choices={(0, 'low'), (1, 'mid'), (2, 'high')}, coerce=int, default=1)
+    due_date = DateField("Due date")
+    priority = SelectField(
+        "Priority", choices={(0, "low"), (1, "mid"), (2, "high")}, coerce=int, default=1
+    )
 
-    comment = TextAreaField('Comment')
+    comment = TextAreaField("Comment")
 
-    ''' Zukunftsmusik ..
+    """ Zukunftsmusik ..
     parent = SelectField('Parent')
     dependson = SelectField('Depends on')
-    '''
+    """
--- todos/models.py
+++ todos/models.py
@@ -1,79 +1,82 @@
-from todos import db, app, mail
+import json
 
-from flask import flash, Markup, url_for
-
+from flask import Markup, flash, url_for
 from flask_login import UserMixin
 from flask_mail import Message
-
-from itsdangerous import URLSafeTimedSerializer as Serializer
 from itsdangerous import BadSignature, SignatureExpired
-
-import json
+from itsdangerous import URLSafeTimedSerializer as Serializer
+from todos import app, db, mail
 
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     login = db.Column(db.String, nullable=False)
-    todos = db.relationship('Todo', backref='user', lazy='dynamic')
+    todos = db.relationship("Todo", backref="user", lazy="dynamic")
 
     def create_token(self):
-        s = Serializer(app.config['SECRET_KEY'])
-        token = s.dumps({'id': self.id})
+        s = Serializer(app.config["SECRET_KEY"])
+        token = s.dumps({"id": self.id})
 
-        if app.config['DEBUG']:
+        if app.config["DEBUG"]:
             flash(
-                Markup('<b>DEBUG:</b> <a href={url}>{url}</a>'.format(
-                    url=url_for('login_with_token', token=token, _external=True))),
-                'warning')
+                Markup(
+                    "<b>DEBUG:</b> <a href={url}>{url}</a>".format(
+                        url=url_for("login_with_token", token=token, _external=True)
+                    )
+                ),
+                "warning",
+            )
             return
 
         # send login email
-        msg = Message('Ohai!', recipients=[self.login])
-        msg.body = 'Here is your login link: {}'.format(url_for('login_with_token', token=token, _external=True))
+        msg = Message("Ohai!", recipients=[self.login])
+        msg.body = "Here is your login link: {}".format(
+            url_for("login_with_token", token=token, _external=True)
+        )
 
         mail.send(msg)
 
     @staticmethod
     def verify_login_token(token):
-        s = Serializer(app.config['SECRET_KEY'])
+        s = Serializer(app.config["SECRET_KEY"])
         try:
-            data = s.loads(token, max_age=10*60)
+            data = s.loads(token, max_age=10 * 60)
         except SignatureExpired:
             # valid token, but expired
             return None
         except BadSignature:
             # invalid token
             return None
-        return User.query.get(data['id'])
+        return User.query.get(data["id"])
 
     def __repr__(self):
-        return '<User {}>'.format(self.login)
+        return "<User {}>".format(self.login)
 
 
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     created = db.Column(db.DateTime, default=db.func.current_timestamp())
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
     title = db.Column(db.String(255))
     description = db.Column(db.Text)
     due_date = db.Column(db.Date)
     state = db.Column(db.Text)
     priority = db.Column(db.Integer)
-    updates = db.relationship('Update', backref='todo', lazy='dynamic')
+    updates = db.relationship("Update", backref="todo", lazy="dynamic")
 
-    ''' Zukunftsmusik
+    """ Zukunftsmusik
     not_before = db.Column(db.Date)
     parent = db.Column(db.Integer, db.ForeignKey('todo.id'))
     depends_on = db.Column(db.Text)
-    '''
+    """
 
 
 class Update(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    todo_id = db.Column(db.Integer, db.ForeignKey('todo.id'), nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    todo_id = db.Column(db.Integer, db.ForeignKey("todo.id"), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
     comment = db.Column(db.Text, nullable=False)
-    __changes = db.Column('changes', db.Text)
+    __changes = db.Column("changes", db.Text)
     created = db.Column(db.DateTime, default=db.func.current_timestamp())
 
     @property
